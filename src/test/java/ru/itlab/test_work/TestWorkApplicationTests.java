574b516585903fa24fbc18fac910e50c6af049d0
koala1101 <rproba@list.ru>
2021-08-23 14:14:39 +0300

    init

---
+++ src/test/java/ru/itlab/test_work/TestWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itlab.test_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
